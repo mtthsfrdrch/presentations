24196e5ffc0b35b318da2fbf30d92c8299e5b8e0
matthias <matthias@greenrobot.de>
2014-11-22 14:43:44 +0100

    Android wear slides for DevFest Berlin 2014

---
+++ 2014_11_GDG_DevFest_Berlin_AndroidWear/slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+  settings: {
+    title: 'Android as it gets worn',
+    subtitle: "November 2014, Berlin",
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    favIcon: 'images/devfest_berlin/devfest_berlin_logo.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    theme: ['gdgbremen'], 
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Matthias Friedrich',
+    twitter: '@peacei',
+    mail: 'mtthsfrdrch@gmail.com',
+    company: 'Jimdo GmbH',
+    gplus: 'google.com/+MatthiasFriedrich'
+  }]
+};
+
