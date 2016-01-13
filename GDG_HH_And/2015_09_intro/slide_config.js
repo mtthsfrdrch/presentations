c86ae40f9e22c0e388f82dc493edac7f6b6f8188
Matthias <mtthsfrdrch@gmail.com>
2016-01-13 23:42:08 +0100

    add GDG HH Android intro slides

---
+++ GDG_HH_And/2015_09_intro/slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+  settings: {
+    title: 'Android Meetup',
+    subtitle: "September 2015, Hamburg",
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    favIcon: 'images/gdg-hamburg-android/GDG_Android Hamburg_280_square.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    theme: ['gdgbremen'], 
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Matthias Friedrich',
+    twitter: '@mtthsfrdrch',
+    mail: 'mtthsfrdrch@gmail.com',
+    company: 'Jimdo GmbH',
+    gplus: 'google.com/+MatthiasFriedrich'
+  }]
+};
+
