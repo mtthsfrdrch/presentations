72a2ef8c3ba6956873125f1c8f0cf9ee32780bde
Matthias <mtthsfrdrch@gmail.com>
2016-06-17 15:12:47 +0200

    Add Droidcon Berlin 2016 CoordinatorLayout Slides

---
+++ 2016_06_DroidconDE_CoordinatorLayout/slide_config.js
@@ -0,0 +1,25 @@
+var SLIDE_CONFIG = {
+  settings: {
+    title: 'Are you coordinating already?',
+    subtitle: "June 2016, Berlin",
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    favIcon: 'images/DroidCon_Logo.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    theme: ['gdgbremen'],
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Matthias Friedrich',
+    twitter: '@mtthsfrdrch',
+    mail: 'mtthsfrdrch@gmail.com',
+    company: 'Jimdo GmbH',
+    gplus: 'google.com/+MatthiasFriedrich'
+  }]
+};
