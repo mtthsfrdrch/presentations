fd69623a4a2ab7c86ada121aafd852a8eb9706c4
Matthias <mtthsfrdrch@gmail.com>
2018-07-20 03:54:37 +0200

    devfest17 welcome slides day 2

---
+++ 2017_11_DevFestHH17_welcome_day2/slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+  settings: {
+    title: 'GDG DevFest Hamburg 2017',
+    subtitle: "November 2017",
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    favIcon: 'images/GDG_Hamburg_132_square.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    theme: ['gdgbremen'], 
+  },
+
+  // Author information
+  presenters: [{
+    name: "",
+    twitter: '',
+    mail: '',
+    company: '',
+    gplus: ''
+  }]
+};
+
